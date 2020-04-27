6defae7a49afd06baedc39ffc01996ad363da545
Marfa Lamova <invisible.kichi@gmail.com>
2020-04-27 10:25:17 +0600

    Entites | Navigation Bar

---
+++ app/src/main/java/com/example/soultosoul/Entities/Blog.java
@@ -0,0 +1,63 @@
+package com.example.soultosoul.Entities;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+//Sts_blog(id, title, content, user_id)
+public class Blog {
+
+    @SerializedName("id")
+    @Expose
+    private long id;
+
+    @SerializedName("title")
+    @Expose
+    private String title;
+
+    @SerializedName("content")
+    @Expose
+    private String content;
+
+    @SerializedName("user_id")
+    @Expose
+    private long user_id;
+
+    public Blog(String title,  String content, long user_id) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.user_id = user_id;
+    }
+
+    public long getId() {
+        return this.id;
+    }
+
+    public void setId( long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public void setTitle( String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return this.content;
+    }
+
+    public void setContent( String content) {
+        this.content = content;
+    }
+
+    public long getUser_id() {
+        return this.user_id;
+    }
+
+    public void setUser_id( long user_id) {
+        this.user_id = user_id;
+    }
+}
---
+++ app/src/main/java/com/example/soultosoul/ProfileActivity.java
@@ -0,0 +1,20 @@
+package com.example.soultosoul;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.r0adkll.slidr.Slidr;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+            }
+        };
